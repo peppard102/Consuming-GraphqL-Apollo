a175cc2cb6843bc776ee552ee4d9d00db2771e3f
Lauren Peppard <LaurenP@ashn.com>
2021-12-22 15:31:10 -0800

    Displaying speakers

--- app/src/pages/conference/Speakers.jsx
+++ app/src/pages/conference/Speakers.jsx
@@ -1,71 +1,92 @@
 import * as React from "react";
 import "./style-sessions.css";
+import { gql, useQuery } from "@apollo/client";
 
 /* ---> Define queries, mutations and fragments here */
+const SPEAKERS = gql`
+  query speakers {
+    speakers {
+      id
+      name
+      bio
+      sessions {
+        id
+        title
+      }
+    }
+  }
+`;
 
 const SpeakerList = () => {
-
   /* ---> Replace hardcoded speaker values with data that you get back from GraphQL server here */
+  const { loading, error, data } = useQuery(SPEAKERS);
   const featured = false;
 
-  return (
-		<div
-      key={'id'}
+  if (loading) return <p>Loading speakers...</p>;
+
+  if (error) return <p>Error loading speakers...</p>;
+
+  return data.speakers.map(({ id, name, bio, sessions }) => (
+    <div
+      key={id}
       className="col-xs-12 col-sm-6 col-md-6"
       style={{ padding: 5 }}
     >
       <div className="panel panel-default">
         <div className="panel-heading">
-          <h3 className="panel-title">{'Speaker: '}</h3>
+          <h3 className="panel-title">Speaker: {name}</h3>
         </div>
         <div className="panel-body">
-          <h5>{'Bio: ' }</h5>
+          <h5>Bio: {bio}</h5>
         </div>
         <div className="panel-footer">
           <h4>Sessions</h4>
-					{
-						/* ---> Loop through speaker's sessions here */
-					}
-          <span>	
-            <button	
-              type="button"	
-              className="btn btn-default btn-lg"	
-              onClick={()=> {
+          {sessions.map((session) => (
+            <span key={session.id}>
+              <p>{session.title}</p>
+            </span>
+          ))}
+          <span>
+            <button
+              type="button"
+              className="btn btn-default btn-lg"
+              onClick={() => {
                 /* ---> Call useMutation's mutate function to mark speaker as featured */
-              }}	
-              >	
-                <i	
-                  className={`fa ${featured ? "fa-star" : "fa-star-o"}`}	
-                  aria-hidden="true"	
-                  style={{	
-                    color: featured ? "gold" : undefined,	
-                  }}	
-                ></i>{" "}	
-                Featured Speaker	
-            </button>	
+              }}
+            >
+              <i
+                className={`fa ${featured ? "fa-star" : "fa-star-o"}`}
+                aria-hidden="true"
+                style={{
+                  color: featured ? "gold" : undefined,
+                }}
+              ></i>{" "}
+              Featured Speaker
+            </button>
           </span>
         </div>
       </div>
     </div>
-	);
+  ));
 };
 
 const SpeakerDetails = () => {
-
-    /* ---> Replace hardcoded speaker values with data that you get back from GraphQL server here */
+  /* ---> Replace hardcoded speaker values with data that you get back from GraphQL server here */
   return (
-    <div key={'id'} className="col-xs-12" style={{ padding: 5 }}>
+    <div key={"id"} className="col-xs-12" style={{ padding: 5 }}>
       <div className="panel panel-default">
         <div className="panel-heading">
-          <h3 className="panel-title">{'name'}</h3>
+          <h3 className="panel-title">{"name"}</h3>
         </div>
         <div className="panel-body">
-          <h5>{'bio'}</h5>
+          <h5>{"bio"}</h5>
         </div>
         <div className="panel-footer">
-          {{
-						/* ---> Loop through speaker's sessions here */
-					}}
+          {
+            {
+              /* ---> Loop through speaker's sessions here */
+            }
+          }
         </div>
       </div>
     </div>
@@ -84,7 +105,6 @@ export function Speaker() {
   );
 }
 
-
 export function Speakers() {
   return (
     <>
@@ -96,5 +116,3 @@ export function Speakers() {
     </>
   );
 }
-
-	
